33888511c324156a5c14eca808bdd50c35fc23d3
Luke Stephens <imahowiie@gmail.com>
2021-08-12 20:45:28 +0300

    Nav Mobile Fix

--- comps/nav/Navbar.js
+++ comps/nav/Navbar.js
@@ -4,69 +4,59 @@ import styles from "../../styles/nav.module.scss";
 
 export default function Navbar() {
   return (
-    <div className={styles.navMain}>
-      <ul>
-        <Link href="/">
-          <li>
-            <div>
-              <Image
-                src="/Images/emojis/house.png"
-                layout="fill"
-              ></Image>
-            </div>
-            <p>Home</p>
-          </li>
-        </Link>
-        
-        <Link href="/about/about">
-          <li>
-            <div>
-              <Image
-                src="/Images/emojis/student.png"
-                layout="fill"
-              ></Image>
-            </div>
-            <p>About</p>
-          </li>
-        </Link>
+    <div className={styles.navCon}>
+      <div className={styles.navMain}>
+        <ul>
+          <Link href="/">
+            <li>
+              <div>
+                <Image src="/Images/emojis/house.png" layout="fill"></Image>
+              </div>
+              <p>Home</p>
+            </li>
+          </Link>
 
-        <Link href="/myWork/myWork">
+          <Link href="/about/about">
+            <li>
+              <div>
+                <Image src="/Images/emojis/student.png" layout="fill"></Image>
+              </div>
+              <p>About</p>
+            </li>
+          </Link>
+
+          <Link href="/myWork/myWork">
+            <li>
+              <div>
+                <Image src="/Images/emojis/mac.png" layout="fill"></Image>
+              </div>
+              <p>Work</p>
+            </li>
+          </Link>
+
           <li>
-            <div>
-              <Image
-                src="/Images/emojis/mac.png"
-                layout="fill"
-              ></Image>
-            </div>
-            <p>Work</p>
+            <a
+              className={styles.blogLi}
+              target="_blank"
+              href="https://medium.com/@LukeStephens"
+            >
+              <div>
+                <Image src="/Images/emojis/book.png" layout="fill"></Image>
+              </div>
+              <p>Blog</p>
+            </a>
           </li>
-        </Link>
 
-        <li>
-        <a className={styles.blogLi} target="_blank" href="https://medium.com/@LukeStephens">
-            <div>
-              <Image
-                src="/Images/emojis/book.png"
-                layout="fill"
-              ></Image>
-            </div>
-            <p>Blog</p>
-        </a>
-        </li>
-
-        <Link href="/products/products">
-          <li>
-            <div>
-              <Image
-                src="/Images/emojis/box.png"
-                layout="fill"
-              ></Image>
-            </div>
-            <p>Products</p>
-          </li>
-        </Link>
-
-      </ul>
+          <Link href="/products/products">
+            <li>
+              <div>
+                <Image src="/Images/emojis/box.png" layout="fill"></Image>
+              </div>
+              <p>Products</p>
+            </li>
+          </Link>
+        </ul>
+      </div>
     </div>
   );
 }
